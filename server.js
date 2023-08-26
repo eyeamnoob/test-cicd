b30e54e256c58c4e23dcf674a4d8a7f7312c0c0f
ali afraz <a.afraz.1380@gmail.com>
2023-08-26 12:21:38 +0330

    add cicd

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const LIARA_URL = process.env.LIARA_URL || "localhost";
+const LIARA_URL = process.env.LIARA_URL || "0.0.0.0";
 
 app.use(express.static("public"));
 
@@ -9,6 +9,6 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.listen(3005, () =>
+app.listen(3000, () =>
   console.log(`app listening on port 3005 on ${LIARA_URL}`)
 );
